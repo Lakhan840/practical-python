7842de40d1032a8eb5f8ac0fd45d9debebea6f35
Lakhan840 <72196041+Lakhan840@users.noreply.github.com>
2020-10-02 18:40:11 +0530

    Create Working_with_data_basics.py
    Added some datatypes basics and exercises

---
+++ Notes/02_Working_with_data/Working_with_data_basics.py
@@ -0,0 +1,31 @@
+# Before you started working with data you have to learn something about datatypes
+
+# First we learn about Built-in Data Types in Python
+
+# Data type is an important concept in every programming language
+
+# because in programming language because Variables store types, and different data types do different things.
+
+# Python has the following built-in  data types 
+
+# if you want working with then use Numebers DataTypes:	int, float, complex
+# if you want working with then use Text DataType:	'str'
+# if you want working with then use Boolean DataTypes:	bool
+# if you want working with then use Sequences DataTypes:	list, tuple, range
+# if you want working with then use Sets DataTypes:	set, frozenset
+# if you want working with then use Mapping DataTypes:	dict
+# if you want working with then use Binary DataTypes:	bytes, bytearray, memoryview
+
+''' How to find variable datatype in python
+''' use this method''''
+ my_datatype = 89 # or any datatype
+print(type(my_datatype))
+
+
+# Exercise find the Datatype of following code 
+
+Data = "Datatypes"
+Data = 20
+Data = 23.56
+Data = 'Hello'
+x = set("Oscar","mike","harry")
